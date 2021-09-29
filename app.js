386a3f9ebca23987e50f93c9cbb2cd66f12a708d
Shaik Zabihulla <informshaikz@gmail.com>
2021-09-30 01:37:19 +0530

    added styles

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
